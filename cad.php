12d52d88c7898ec06b30d8c15ba69a5851802be9
raylsoneli12 <raylsonelizeu@gmail.com>
2024-10-18 19:26:15 -0300

    Add files via upload

---
+++ cad.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style-guanabara.css">
+</head>
+<body>
+    <header><h1>Respostas</h1></header>
+    <main>
+        <section>
+            <?php 
+            echo "<h2>Foda-se seu nome!</h2>";
+            
+            ?>
+            <a href="./index.html"><button>🔙Voltar</button></a>
+        </section>
+    </main>
+    
+</body>
+</html>
